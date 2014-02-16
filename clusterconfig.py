096b6b3130365a5fd53b4efc3f09e0b60b330575
Chris Friedline <cfriedline@vcu.edu>
2014-02-16 11:25:38 -0500

    add cluster config

---
+++ clusterconfig.py
@@ -0,0 +1,15 @@
+from kombu import Queue
+
+BROKER_URL = 'amqp://guest@godel97'
+CELERY_RESULT_BACKEND = 'amqp://'
+CELERY_TASK_SERIALIZER = 'pickle'
+CELERY_RESULT_SERIALIZER = 'pickle'
+CELERY_ACCEPT_CONTENT=['pickle', 'json']
+CELERY_TIMEZONE = 'America/New York'
+CELERY_ENABLE_UTC = True
+CELERYD_PREFETCH_MULTIPLIER = 1
+
+CELERY_QUEUES = (
+    Queue('celery', routing_key='celery'),
+    Queue('transient', routing_key='transient', delivery_mode=1))
+
