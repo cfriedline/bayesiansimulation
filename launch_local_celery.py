6fec07ff0a49e73dd69fa0e36f676e1f044c8208
Chris Friedline <cfriedline@vcu.edu>
2014-02-18 09:33:12 -0500

    add script to launch local celery cluster

---
+++ launch_local_celery.py
@@ -0,0 +1,24 @@
+from fabric.operations import local
+import multiprocessing as mp
+import time
+
+rabbit = "/Users/chris/src/rabbitmq_server-3.2.3/sbin/rabbitmq-server"
+worker = "celery worker -A runsimulation4 -l info"
+
+def run_task(task):
+    return local(task)
+
+def main():
+    jobs = []
+    pool = mp.Pool(2)
+    jobs.append(pool.apply_async(run_task, args=(rabbit,)))
+    time.sleep(2)
+    jobs.append(pool.apply_async(run_task, args=(worker,)))
+
+    while(True):
+        for j in jobs:
+            print j.get()
+        time.sleep(2)
+
+if __name__ == '__main__':
+    main()
